eabe117097381fbd759897ba0e745f28c98c38b0
Md farhaf alom <88694159+farhadalom@users.noreply.github.com>
2021-08-15 09:20:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+}
